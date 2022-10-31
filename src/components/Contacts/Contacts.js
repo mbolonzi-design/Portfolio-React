71a97aea6dc1dda2088d9fe2e7d2f52638bab6fd
mbolonzi-design <mbolonzi.design@gmail.com>
2022-10-31 18:39:09 +0300

    style: style contact section and add more info

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {FaGithub, FaLinkedin, FaTwitter} from 'react-icons/fa';
 import emailjs from 'emailjs-com';
 import {Form, Input, Button, TextArea} from 'semantic-ui-react';
 import Swal from 'sweetalert2';
@@ -36,47 +37,60 @@ function Contacts () {
 
 
   return (
-    <div className='contact-form'>
-      <div className='email'>
-        <strong>Email:</strong> mbolonzi.design@gmail.com
+    <div id='contact'>
+      <section className='contact-sect'>
+      <div className='form-container'>
+        <h4>Send A Message</h4>
+        <form onSubmit={sendEmail}>
+          <Form.Field
+            id='form-input-control-email'
+            control={Input}
+            label='Email'
+            name='user_email'
+            placeholder='Email'
+            type='email'
+            icom='mail'
+            iconPosition='left'
+          />
+          <Form.Field
+            id='form-input-control-first-name'
+            control={Input}
+            label='Name'
+            name='user_name'
+            placeholder='Name'
+            required
+            icon='user circle'
+            iconPosition='left'
+          />
+          <Form.Field
+            id='form-textarea-control-opinion'
+            control={TextArea}
+            label='Message'
+            name='user_message'
+            placeholder='Message'
+            required
+          />
+          <Button type='submit' color='green'>Send</Button>
+        </form>
       </div>
-      <div className='phone'>
-        <strong>Phone:</strong> +254 746 488 732
+      <div className='socials'>
+        <h4>Let's Get Social</h4>
+        <p>
+          Whether you want to talk about a project collaboration, or just say hi, I'd love to hear from you.Simply fill the form and send me an email or send a DM on socials below.
+        </p>
+        <div className='social-icons'>
+          <a href='https://github.com/mbolonzi-design' target='_blank' rel='noreferrer'>
+            <FaGithub className='icon github' />
+          </a>
+          <a href='https://www.linkedin.com/in/mbolonzi-peter/' target='_blank' rel='noreferrer'>
+            <FaLinkedin className='icon linkedin' />
+          </a>
+          <a href='https://twitter.com/mbolonzi_' target='_blank' rel='noreferrer'>
+            <FaTwitter className='icon twitter' />
+          </a>
+        </div>
       </div>
-      <div>
-        <h4>Leave me a message...</h4>
-      </div>
-      <form onSubmit={sendEmail}>
-        <Form.Field
-          id='form-input-control-email'
-          control={Input}
-          label='Email'
-          name='user_email'
-          placeholder='Email'
-          type='email'
-          icom='mail'
-          iconPosition='left'
-        />
-        <Form.Field
-          id='form-input-control-first-name'
-          control={Input}
-          label='Name'
-          name='user_name'
-          placeholder='Name'
-          required
-          icon='user circle'
-          iconPosition='left'
-        />
-        <Form.Field
-          id='form-textarea-control-opinion'
-          control={TextArea}
-          label='Message'
-          name='user_message'
-          placeholder='Message'
-          required
-        />
-        <Button type='submit' color='green'>Send</Button>
-      </form>
+      </section>
     </div>
   )
 }
