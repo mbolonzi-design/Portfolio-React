d7a149711430a515f0d1d5e2d1ef33a0f0a51454
mbolonzi-design <mbolonzi.design@gmail.com>
2022-10-31 10:25:37 +0300

    add home background image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,15 +3,23 @@ import './Home.css';
 
 function Home() {
     return (
-        <div className='intro'>
-            <h3>Hi, there</h3>
-            <p>
-                I am a software developer with a passion for building web applications.
-                I am also a graduate of the <a href="https://moringaschool.com/" target="_blank" rel="noopener noreferrer">Moringa School</a> Full Stack Web Development program.
-                I am a self-taught programmer with a background in Computer Science and a passion for learning.
-                I am always looking for new opportunities to learn and grow as a developer.
-                If you are interested in working with me, please feel free to contact me.
-            </p>
+        <div 
+            className='hero' 
+            style={{
+                backgroundImage: "url(/images/bg03.jpg)",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                height: "100vh",
+                width: "100vw",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column"
+            }}
+        >
+           <h3>Hi, I'm Mbolonzi</h3>
+           <p>A software engineer from Nairobi, Kenya</p>
         </div>
     );
 }
