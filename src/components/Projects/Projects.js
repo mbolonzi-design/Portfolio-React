806bb90131527bbdc36bc96659c274a504bde0f1
mbolonzi-design <mbolonzi.design@gmail.com>
2022-10-31 10:27:18 +0300

    setup projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,24 @@
 import React from "react";
-
+import './Projects.css';
 function Projects() {
   return (
     <div>
-      <h1>Projects</h1>
+      <h4>Projects</h4>
+      <div className="project">
+        <div className="project-image">
+          <img src="/images/bg03.jpg" alt="project" />
+          <div className="desc">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi, nisi?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, tempore.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, dolore.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, illum.
+            </p>
+          </div>
+          <img src="/images/bg04.jpg" alt="bg2" />
+          <img src="/images/bg06.jpg" alt="bg4" />
+        </div>
+      </div>
     </div>
   );
 }
