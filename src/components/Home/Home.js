5f0f12ce67d3368ff7892e98811e702300f51d42
mbolonzi-design <mbolonzi.design@gmail.com>
2022-10-31 22:38:34 +0300

    add social accounts on home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {FaGithub, FaLinkedin, FaTwitter} from 'react-icons/fa';
 import './Home.css';
 
 function Home() {
@@ -20,6 +21,17 @@ function Home() {
         >
            <h3>Hi, I'm Mbolonzi</h3>
            <p>A software engineer from Nairobi, Kenya</p>
+           <div className="get-social">
+                <a href='https://github.com/mbolonzi-design' target='_blank' rel='noreferrer'>
+                    <FaGithub className='icon github' />
+                </a>
+                <a href='https://www.linkedin.com/in/mbolonzi-peter/' target='_blank' rel='noreferrer'>
+                    <FaLinkedin className='icon linkedin' />
+                </a>
+                <a href='https://twitter.com/mbolonzi_' target='_blank' rel='noreferrer'>
+                    <FaTwitter className='icon twitter' />
+                </a>
+           </div>
         </div>
     );
 }
