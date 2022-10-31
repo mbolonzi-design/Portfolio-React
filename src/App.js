bbd43ceeb42075610799b1819f4e88a6c75ed8a6
mbolonzi-design <mbolonzi.design@gmail.com>
2022-10-31 18:40:23 +0300

    remove navbar temporarily

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+//import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Contacts from './components/Contacts/Contacts';
-import NavBar from './components/NavBar/NavBar';
+//import NavBar from './components/NavBar/NavBar';
 import './App.css';
 //import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div>
-      <Router>
+      < Home />
+      <hr />
+      < About />
+      <hr />
+      < Projects />
+      <hr />
+      < Contacts />
+
+      {/* <Router>
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          {/* <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/contacts" element={<Contacts />} />
-        </Routes>
+          <Route path="/contacts" element={<Contacts />} /> */}
+        {/* </Routes> */}
         {/* <Footer /> */}
-      </Router>
+        {/* // <About /> */}
+        {/* // <Projects /> */}
+        {/* // <Contacts /> */}
+      {/* </Router> */} 
     </div>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,10 +7,10 @@ function NavBar() {
         <div>
             <nav>
                 <ul>
-                    <li><NavLink exact to="/">Home</NavLink></li>
-                    <li><NavLink exact to="/about">About</NavLink></li>
+                    <li><NavLink exact to="/">Portfolio</NavLink></li>
+                    {/* <li><NavLink exact to="/about">About</NavLink></li>
                     <li><NavLink exact to="/projects">Projects</NavLink></li>
-                    <li><NavLink exact to="/contacts">Contacts</NavLink></li>
+                    <li><NavLink exact to="/contacts">Contacts</NavLink></li> */}
                 </ul>
             </nav>
         </div>
