34e27ece64713ab76c6b96c5da3e4c5907a6abef
mbolonzi-design <mbolonzi.design@gmail.com>
2022-10-31 10:24:55 +0300

    update and style about component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,23 @@
 import React from "react";
+import './About.css';
 
 function About() {
   return (
     <div>
-      <h3>About Me</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-      </p>
+        <div id="profile">
+            <div className="profile-image">
+              <img src="/images/me.jpg" alt="Mbolonzi" />
+            </div>
+            <div className="desc">
+              <p>
+                  I am a software developer from with a passion for building web applications.
+                  I am also a graduate of the <a href="https://moringaschool.com/" target="_blank" rel="noopener noreferrer">Moringa School</a> Full Stack Web Development program.
+                  I have a background in Computer Science. I enjoy solving real world problems with some lines of code.
+                  I am always looking for new opportunities to learn and grow as a developer.
+                  If you are interested in working with me, please feel free to contact me.
+              </p>
+            </div>
+        </div>
     </div>
   );
 }
